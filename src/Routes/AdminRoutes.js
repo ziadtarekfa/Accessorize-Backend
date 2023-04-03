d26dc57861deaee8bc2647f7e25102600767a4e7
Ziad Tarek <ziadtarekfa@gmail.com>
2023-04-03 23:36:23 +0200

    changed post method to delete

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {login,logout,getAdmins, addSeller,signUp,getSellers,getUsers, deleteUser, deleteAdmin, numberOfUsers, numberOfAdmins, numberOfSellers, deleteSeller, updateSeller, updateUser, recentUsers, recentSellers} = require('../Controller/adminController')
+const { login, logout, getAdmins, addSeller, signUp, getSellers, getUsers, deleteUser, deleteAdmin, numberOfUsers, numberOfAdmins, numberOfSellers, deleteSeller, updateSeller, updateUser, recentUsers, recentSellers } = require('../Controller/adminController')
 const router = express.Router();
 
 
@@ -8,7 +8,7 @@ router.post('/signup', signUp)
 
 router.get('/logout', logout);
 router.get('/admins', getAdmins);
-router.post('/addseller',addSeller);
+router.post('/addseller', addSeller);
 router.get('/sellers', getSellers);
 router.get('/recentUsers', recentUsers);
 router.get('/recentSellers', recentSellers);
@@ -16,9 +16,9 @@ router.get('/users', getUsers);
 router.get('/usersCount', numberOfUsers);
 router.get('/adminsCount', numberOfAdmins);
 router.get('/sellersCount', numberOfSellers);
-router.post('/deleteUser', deleteUser);
-router.post('/deleteSeller', deleteSeller);
+router.delete('/deleteUser', deleteUser);
+router.delete('/deleteSeller', deleteSeller);
 router.post('/deleteAdmin', deleteAdmin);
-router.post('/updateSeller',updateSeller)
-router.post('/updateUser',updateUser)
+router.post('/updateSeller', updateSeller)
+router.post('/updateUser', updateUser)
 module.exports = router;
