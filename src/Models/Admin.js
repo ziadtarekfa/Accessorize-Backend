21c67c2f0d860adf9e8b02a2a2149ad987035109
Ziad Tarek <ziadtarekfa@gmail.com>
2023-06-26 04:25:40 +0300

    changed admin email to be unique

--- src/Models/Admin.js
+++ src/Models/Admin.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {ObjectId} = mongoose.Schema;
 
 const adminSchema = new Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false
-    },
-    password: {
-      type: String,
-      required: true
-    },
-  
-  }, { timestamps: true });
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
-  const Admin = mongoose.model("Admin" , adminSchema);
-  module.exports= Admin;
-  
+const Admin = mongoose.model("Admin", adminSchema);
+module.exports = Admin;
