e9189edcec5916b9c4e0f42d90ac6027cc2e71b6
Ziad Tarek <ziadtarekfa@gmail.com>
2023-09-07 19:06:21 +0300

    removed unused code

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const { signUp, logout, getUsers, login,searchProduct,addToFavourites,makeAnOrder,updateUser,deleteUser,deleteFromFavorites, getProducts, getCategorizedProducts }=require('../Controller/userController');
-//const {addItemToCart,getItemsInCart} = require('../Controller/cartController')
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post('/login', login)
 router.get('/logout', logout);
 router.get('/getusers', getUsers);
-// router.post('/addItemToCart',addItemToCart);
-// router.get('/getItemsInCart', getItemsInCart);
-// router.post('/addProductToCart', addProductToCart);
 
 router.get('/searchproduct',searchProduct);
-
 router.post('/favourites',addToFavourites);
 router.post('/order',makeAnOrder);
 router.get('/products',getProducts);
